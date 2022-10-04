7a93f1d804d2c9d531fb91bc4fe115fb7469ab72
tnalss <indoubt@naver.com>
2022-10-04 14:08:09 +0900

    first

--- script/main.js
+++ script/main.js
@@ -1,3 +1,18 @@
 $(function () {
 
 })
+
+window.addEventListener("DOMContentLoaded", function() {
+
+	const FIRST = document.querySelector(".first");
+	const SECOND = document.querySelector(".second");
+	const THIRD = document.querySelector(".third");
+
+    let postIndex = 1;
+	FIRST.addEventListener("click", function(e) {
+        console.log(e);
+
+	})
+
+
+})
